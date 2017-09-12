c88463c4fda8abb1198b4206e3f646cc2a3dbcf7
Daniel Muliawan <daniel.muliawan@mitrais.com>
2017-09-12 11:51:46 +0800

    Lambda Expressions

---
+++ src/Person.java
@@ -0,0 +1,11 @@
+class Person {
+	String name;
+	String grade;
+	Integer age;
+	
+	public Person(String name, Integer age, String grade) {
+		this.name = name;
+		this.age = age;
+		this.grade = grade;
+	}
+}
